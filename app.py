65064ef3ec76c6eaf731917614d7e41e587dac99
FarizioKautsar <kheruzy@gmail.com>
2025-09-02 10:32:41 +1000

    Cors stuffs

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from recipes.recipe_api import blp as RecipesBlueprint
 from mood.mood_api import blp as MoodBlueprint
 from children_info.children import blp as ChildBlueprint
 import config
+from flask_cors import CORS
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -16,6 +18,14 @@ def create_app():
     api.register_blueprint(RecipesBlueprint)
     api.register_blueprint(MoodBlueprint)
     api.register_blueprint(ChildBlueprint)
+    app.url_map.strict_slashes = False
+    
+    frontend_urls = os.getenv("FRONTEND_URL", "http://localhost:8081").split(",")
+    CORS(
+        app,
+        origins=frontend_urls,
+        supports_credentials=True
+    )
 
     return app
 
